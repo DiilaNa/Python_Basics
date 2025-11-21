8f4804e5b90beb4a28c573f513664319740ca409
DiilaNa <liyanaarachchidilan@gmail.com>
2025-11-21 11:41:28 +0530

    Packages

---
+++ Package_testing/Main.py
@@ -0,0 +1,29 @@
+
+# Method One - import My_Calculator.addition, My_Calculator.substraction
+# Method Two - from My_Calculator import addition , substraction
+# Method Three - from My_Calculator.addition import add , substraction
+
+# import My_Calculator.addition, My_Calculator.substraction
+
+# print(My_Calculator.addition.add(1,3))
+# print(My_Calculator.substraction.substraction(5,2))
+
+# from My_Calculator.addition import add
+
+# print(add (2,6))
+
+
+# from My_Calculator  import addition
+
+# print(addition.add(4,6))
+
+from My_Calculator import add
+
+print(add(4,5))
+
+
+
+
+
+
+
